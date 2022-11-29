678d2b912fde5429e08629235654190add72d638
Sam Azimi <samzih1337@gmail.com>
2022-11-29 22:24:53 +0100

    Initial commit

---
+++ Product.php
@@ -0,0 +1,15 @@
+<?php
+
+
+class Product
+{
+    private $id;
+    private $title;
+    private $price;
+    private $inStock;
+
+    // TODO Skriv en konstruktor som sätter alla properties
+
+    // TODO Skriv getters för alla properties
+
+}
